5027a46e4400c86725fb06230f76a6fb3ffd71a5
mandrykina <Kristys@list.ru>
2023-07-11 00:47:42 +0300

    дз 2

--- tests/test_todos_operations.py
+++ tests/test_todos_operations.py
@@ -1,6 +1,6 @@
 import os
 
-from selene import browser, have
+from selene import browser, have, command
 
 
 # Заполнение и отправка формы
@@ -8,22 +8,24 @@ from selene import browser, have
 def test_fill_out_and_submit_the_form(setup_browser):
     browser.open('/automation-practice-form')
 
+
     browser.element('#firstName').type('Alex')
     browser.element('#lastName').type('Bell')
     browser.element('#userEmail').type('alexbell@gmail.com')
-    #browser.element('[name=gender][value=Male]+label').click()
+    # browser.element('[name=gender][value=Male]+label').click()
     browser.all('#genterWrapper .custom-control').element_by(have.exact_text('Male')).click()
     browser.element('#userNumber').type('89998889988')
     browser.element('#dateOfBirthInput').click()
-    #browser.element('.react-datepicker__month-select').click()
-    #browser.element('.react-datepicker__month-select').element('option[value="3"]').click()
+    # browser.element('.react-datepicker__month-select').click()
+    # browser.element('.react-datepicker__month-select').element('option[value="3"]').click()
     browser.element('.react-datepicker__month-select').type('April')
-    #browser.element('.react-datepicker__year-select').click()
-    #browser.element('.react-datepicker__year-select').element('option[value="1992"]').click()
+    # browser.element('.react-datepicker__year-select').click()
+    # browser.element('.react-datepicker__year-select').element('option[value="1992"]').click()
     browser.element('.react-datepicker__year-select').type('1992')
-    browser.element('.react-datepicker__day--0{10}').click()
+    browser.element('.react-datepicker__day--010').click()
+    browser.element('#subjectsInput').perform(command.js.scroll_into_view)
     browser.element('#subjectsInput').type('Computer Science').press_enter()
-    #browser.element('label[for=hobbies-checkbox-1]').click()
+    # browser.element('label[for=hobbies-checkbox-1]').click()
     browser.all('#hobbiesWrapper .custom-control').element_by(have.exact_text('Sports')).click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('Picture/picture.jpg'))
     browser.element('#currentAddress').type('110006 Kashmiri Gate')
@@ -31,6 +33,8 @@ def test_fill_out_and_submit_the_form(setup_browser):
     # browser.all("#state div").element_by(have.exact_text("NCR")).click()
     browser.all('[id^=react-select][id*=option]').element_by(have.exact_text("NCR")).click()
     browser.element('#city').click()
-    #browser.all("#city div").element_by(have.exact_text("Delhi")).click()
+    # browser.all("#city div").element_by(have.exact_text("Delhi")).click()
     browser.all('[id^=react-select][id*=option]').element_by(have.exact_text("Delhi")).click()
+
+
 
